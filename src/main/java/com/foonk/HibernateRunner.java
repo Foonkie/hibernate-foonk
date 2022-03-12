dba51254c9e9bf7727442bd762a97d60966d1d0b
Foonkie <foonk@inbox.ru>
2022-03-12 23:49:16 +0300

    18 ver.

--- src/main/java/com/foonk/HibernateRunner.java
+++ src/main/java/com/foonk/HibernateRunner.java
@@ -10,6 +10,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.graph.GraphSemantic;
 
+import javax.persistence.LockModeType;
 import java.sql.SQLException;
 import java.util.Map;
 
@@ -18,16 +19,20 @@ public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException {
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            try {
-            Transaction transaction = session.beginTransaction();
-            var payment1 = session.find(Payment.class, 1L);
-            var payment2 = session.find(Payment.class, 2L);
-            session.getTransaction().commit();}
-            catch (Exception exception){
-                session.getTransaction().rollback();
-                throw exception;
-            }
+             Session session = sessionFactory.openSession();
+             Session session1 = sessionFactory.openSession())
+        {
+
+
+            session.beginTransaction();
+            session1.beginTransaction();
+            Payment payment = session.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+            payment.setAmount(payment.getAmount()+10);
+            Payment theSamePayment = session1.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+            theSamePayment.setAmount(theSamePayment.getAmount()+20);
+            session.getTransaction().commit();
+
+
         }
     }
 }
